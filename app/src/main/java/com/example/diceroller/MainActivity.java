55f7d2c3aa1dca3a90c1787f8a16e4661d836149
GurinderBandesha <1813902@brunel.ac.uk>
2019-11-06 21:29:28 +0000

    Updated with d ice

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -13,8 +13,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import java.util.Random;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +39,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
+
+
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void on_button_click(View view) {
 
+        TextView dupdated = this.findViewById(R.id.Icebreaker_textview);
 
         EditText userin = this.findViewById(R.id.chosennumber);
 
@@ -69,18 +86,20 @@ public class MainActivity extends AppCompatActivity {
 
         TextView updated = this.findViewById(R.id.textView5score);
 
-
-
         TextView tv = this.findViewById(R.id.textView);
+
+        int max = 6;
+        int min = 1;
+
+
         Random r = new Random();
-        int number = r.nextInt(6);
-
-
-        String user = userin.getText().toString();
-        int finalValue = Integer.parseInt(user);
+        int number = r.nextInt(max - min + 1) + min;
 
 
         tv.setText(Integer.toString(number));
+
+        String user = userin.getText().toString();
+        int finalValue = Integer.parseInt(user);
 
 
         if (finalValue > 0 || finalValue < 7) {
@@ -90,23 +109,106 @@ public class MainActivity extends AppCompatActivity {
                 change.setText(("Congrats"));
                 userin.setText("");
 
-                scored = scored+1;
+                scored = scored + 1;
 
 
-            }
-            else {
-                change.setText("Enter a number 0-6");
+            } else {
+                change.setText("Incorrect");
                 userin.setText("");
 
 
             }
-        }
-            else{
+        } else {
 
-            }
-            updated.setText(String.valueOf(scored));
+        }
+        updated.setText(String.valueOf(scored));
+
+
+    }
+
+//        public void d_icebreaker (View view)
+//        {
+//            TextView tv5 = this.findViewById(R.id.Icebreaker_textview);
+//
+//            ArrayList<String> list = new ArrayList<String>(); //create new array
+//            list.add("If you could go anywhere in the world, where would you go?");
+//            list.add("If you were stranded on a desert island, what three things would you want to take with you?");
+//            list.add("If you could eat only one food for the rest of your life, what would that be?");
+//            list.add("If you won a million dollars, what is the first thing you would buy?");
+//            list.add("If you could spaned the day with one fictional character, who would it be?");
+//            list.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+//
+//
+    // }
+
+    public void  d_icebreakerr( View view) {
+
+        TextView dupdated = this.findViewById(R.id.Icebreaker_textview);
+
+
+        int max = 6;
+        int min = 1;
+
+
+        Random r = new Random();
+        int dnumber = r.nextInt(max - min + 1) + min;
+
+
+        if (dnumber == 1) {
+
+            dupdated.setText("If you could go anywhere in the world, where would you go?");
+        }
+
+        else if (dnumber == 2) {
+
+            dupdated.setText("If you were stranded on a desert island, what three things would you want to take with you?");
+        }
+
+        else if (dnumber == 3) {
+
+            dupdated.setText("If you could eat only one food for the rest of your life, what would that be?");
+        }
+
+        else if (dnumber == 4) {
+
+            dupdated.setText("if you won a million dollars, what is the first thing you would buy?");
+
+        }
+        else if (dnumber == 5) {
+
+            dupdated.setText("If you could spaned the day with one fictional character, who would it be?");
+        }
+
+        else if (dnumber == 6) {
+
+            dupdated.setText("If you found a magic lantern and a genie gave you three wishes, what would you wish?");
+        } else {
+            dupdated.setText("Questions");
+
         }
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
